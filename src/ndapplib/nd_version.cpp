821aebbba4b4a1bc31a2bef6b15032128f6fe20f
duan xiuyun <duan@vmlinux.duan>
2020-05-29 18:00:43 +0800

    auto commit

--- src/ndapplib/nd_version.cpp
+++ src/ndapplib/nd_version.cpp
@@ -1,12 +1,10 @@
-//
-//  nd_version.cpp
-//  ndMacStatic
-//
-//  Created by duanxiuyun on 15-1-15.
-//  Copyright (c) 2015 duanxiuyun. All rights reserved.
-//
-
-#include <stdio.h>
-
-const char *__g_version_desc ="version 1.1 \n build time: \n" ;
-int __g_version_id = 101000 ;
+// this is version file auto-create by script 
+const char *__g_version_desc =
+#ifdef ND_DEBUG 
+" DEBUG " 
+#endif 
+" 1.0.22   build time: DATE: 2020-05-29% TIME: 17:53:36 "
+" Author:  duan" 
+" BuildMachine:  vmlinux.duan Linux 3.10.0-1062.el7.x86_64 x86_64"; 
+ 
+int __g_version_id = 100022 ;
