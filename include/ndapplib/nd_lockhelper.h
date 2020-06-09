6a8b56e84c68cf20e7702acd6423a376c48f5dd7
duan <duanxiuyun@aixgame.com>
2020-06-09 11:44:40 +0800

    auto commit

--- include/ndapplib/nd_lockhelper.h
+++ include/ndapplib/nd_lockhelper.h
@@ -16,13 +16,26 @@ class  ND_COMMON_CLASS LockHelper
 public:
 	LockHelper(nd_mutex *lock)
 	{
-		NDCallTrace name("nd_mutex_lock") ;
+		//NDCallTrace name("nd_mutex_lock") ;
 		m_lock = lock ;
 		nd_mutex_lock(lock) ;
 	}
+	
+	LockHelper() :m_lock(0) {}
+
+	inline bool tryLock(nd_mutex *lock)
+	{
+		//NDCallTrace name("nd_mutex_trylock");
+		if (0 == nd_mutex_trylock(lock)) {
+			m_lock = lock;
+			return true;
+		}
+		return false;
+	}
+	inline bool isLocked() { return m_lock ? true : false; }
 	~LockHelper() 
 	{
-		nd_mutex_unlock(m_lock) ;
+		if(m_lock)nd_mutex_unlock(m_lock) ;
 	}
 private:
 	nd_mutex *m_lock ; 
