db8356c4c8c2e40dd78ededc38aba0eee7e553ee
duan <duanxiuyun@aixgame.com>
2020-06-19 14:41:23 +0800

    remove trace message-for-all-time

--- src/nd_net/nd_msgentry.c
+++ src/nd_net/nd_msgentry.c
@@ -495,8 +495,8 @@ int nd_srv_translate_message( nd_netui_handle connect_handle, nd_packhdr_t *msg 
 		}		
 	}
 
-    nd_logmsg("TRACE Message RECV(%d, %d) length=%d %s\n", usermsg->maxid,  usermsg->minid,
-              usermsg->packet_hdr.length, ret==-1?"FAILED":"SUCCESS") ;
+    //nd_logmsg("TRACE Message RECV(%d, %d) length=%d %s\n", usermsg->maxid,  usermsg->minid,
+    //          usermsg->packet_hdr.length, ret==-1?"FAILED":"SUCCESS") ;
     
 #if defined(ND_TRACE_MESSAGE)
     if (node && node->is_log) {
