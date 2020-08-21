0169dc3e15bdc1e41adce27ad336756d3615550f
neil78duan <luckduan@126.com>
2020-08-21 18:27:39 +0800

    auto commit

--- src/ndapplib/nd_version.cpp
+++ src/ndapplib/nd_version.cpp
@@ -1,10 +1,12 @@
-// this is version file auto-create by script 
-const char *__g_version_desc =
-#ifdef ND_DEBUG 
-" DEBUG " 
-#endif 
-" 1.0.85   build time: DATE: 2020-08-17 TIME: 19:15:35 "
-" Author:  duan" 
-" BuildMachine:  MacbookPro-of-duan.local Darwin 18.6.0 x86_64"; 
- 
-int __g_version_id = 100085 ;
+//
+//  nd_version.cpp
+//  ndMacStatic
+//
+//  Created by duanxiuyun on 15-1-15.
+//  Copyright (c) 2015 duanxiuyun. All rights reserved.
+//
+
+#include <stdio.h>
+
+const char *__g_version_desc ="version 1.1 \n build time: \n" ;
+int __g_version_id = 101000 ;
